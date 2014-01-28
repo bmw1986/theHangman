cbc6f062846c2f3a4e8ff8962187fe43c2a533f3
Freddy Oakes <freddy.oakes@gmail.com>
2014-01-28 14:20:44 -0500

    Cleaned up the program...still has bugs

--- Main.java
+++ Main.java
@@ -1,86 +1,57 @@
 package thePackage;
 
-import java.awt.*; 
-import java.awt.event.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.JApplet;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-//import javax.comm.*;
-import java.util.*;
 
 import thePackage.Paint;
 
-@SuppressWarnings("serial")
 
-public class Main extends JApplet implements ActionListener {
+public class Main {
 
-	Button yesButton, noButton;
-	JLabel welcome, areYouSure;
-	
-	public void init() {
-		setBackground(Color.white);
-		setForeground(Color.white);
-		setLayout(new FlowLayout());
-	}
-	
-	public void actionPerformed (ActionEvent evt) {
-		if (evt.getSource() == yesButton) {
-			repaint();
-		} else if (evt.getSource() == noButton) 
-			repaint();
-	}
-	
 	public static void main(String[] args) {
 		
-		// Create the Program Frame
-	    final JFrame theFrame = new JFrame("Hangman for CompE Junior Lab");
-	
-	    final Graphics g = theFrame.getGraphics();
-//	   
-//	    final Graphics2D g2 = (Graphics2D) g;
-//		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//		g2.setPaint(Color.gray);
-	    
-	    //////////////////////// RS232 ////////////////////////////
-	    
-	    
-	    
-	    ///////////////////////////////////////////////////////////
-	    
-	    KeyListener keyListener = new KeyListener() {
+		final JFrame theFrame = new JFrame();
 		
+		Paint.paintStage(null);
+		
+		KeyListener keyListener = new KeyListener() {
+			
 			@Override
 			public void keyReleased(KeyEvent e) {
 				int key = e.getKeyCode();
 				
 				if (key == KeyEvent.VK_1) {
 					System.out.println("You have pressed an \"1\" on the keyboard!");
-					Paint.paintComponent(g);
-//					theFrame.repaint();
+					Paint.paintHead(null);
+					theFrame.repaint();
 				}
 				if (key == KeyEvent.VK_2) {
 					System.out.println("You have pressed an \"2\" on the keyboard!");
-//					g2.draw(new Line2D.Double(320, 220, 320, 340)); // Body
+					Paint.paintBody(null);
+					theFrame.repaint();
 				}
 				if (key == KeyEvent.VK_3) {
 					System.out.println("You have pressed an \"3\" on the keyboard!");
-//					g2.draw(new Line2D.Double(320, 265, 280, 310)); // Left Arm
+					Paint.paintLeftArm(null);
+					theFrame.repaint();
 				}
 				if (key == KeyEvent.VK_4) {
 					System.out.println("You have pressed an \"4\" on the keyboard!");
-//					g2.draw(new Line2D.Double(320, 265, 360, 310)); // Right Arm
+					Paint.paintRightArm(null);
+					theFrame.repaint();
 				}
 				if (key == KeyEvent.VK_5) {
 					System.out.println("You have pressed an \"5\" on the keyboard!");
-//					g2.draw(new Line2D.Double(320, 340, 275, 430)); // Left Leg
+					Paint.paintLeftLeg(null);
+					theFrame.repaint();
 				}
 				if (key == KeyEvent.VK_6) {
 					System.out.println("You have pressed an \"6\" on the keyboard!");
-//					g2.draw(new Line2D.Double(320, 340, 360, 430)); // Right Leg
+					Paint.paintRightLeg(null);
+					theFrame.repaint();
 				}
 			}
 			
@@ -97,18 +68,9 @@ public class Main extends JApplet implements ActionListener {
 		
 		theFrame.addKeyListener(keyListener);
 		
-		theFrame.addWindowListener(new WindowAdapter() {
-	         public void windowClosing(WindowEvent e) {
-	            System.exit(0);
-	         }
-	      });
-		
-	    JApplet applet = new Main();
-	    theFrame.getContentPane().add("Center", applet);
-	    applet.init();
-	      
-	    theFrame.pack();
+		theFrame.pack();
 	    theFrame.setSize(new Dimension(800, 600));
 	    theFrame.setVisible(true);
+
 	}
 }
--- Paint.java
+++ Paint.java
@@ -1,13 +1,16 @@
 package thePackage;
 
-import java.awt.*; 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 
 public class Paint {
 
-	public static void paintComponent (Graphics g) {
-
+	public static void paintStage(Graphics g) {
+		
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setPaint(Color.gray);
@@ -20,21 +23,50 @@ public class Paint {
 		g2.draw(new Line2D.Double(320, 100, 480, 100)); // Top Beam
 		g2.draw(new Line2D.Double(320, 100, 320, 160)); // Rope
 		g2.draw(new Line2D.Double(200, 480, 600, 480));	// Bottom Line
+		
 	}
 	
-	public static void paintHead (Graphics g) {
+	public static void paintHead(Graphics g) {
+		
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setPaint(Color.gray);
-		
 		g2.draw(new Ellipse2D.Double(296, 160, 50, 60)); // Head
 	}
 	
-	public static void paintBody (Graphics g) {
+	public static void paintBody(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setPaint(Color.gray);
-		
 		g2.draw(new Line2D.Double(320, 220, 320, 340)); // Body
 	}
+	
+	public static void paintLeftArm(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setPaint(Color.gray);
+		g2.draw(new Line2D.Double(320, 265, 280, 310)); // Left Arm
+	}
+	
+	public static void paintRightArm(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setPaint(Color.gray);
+		g2.draw(new Line2D.Double(320, 265, 360, 310)); // Right Arm
+	}
+	
+	public static void paintLeftLeg(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setPaint(Color.gray);
+		g2.draw(new Line2D.Double(320, 340, 275, 430)); // Left Leg
+	}
+	
+	public static void paintRightLeg(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setPaint(Color.gray);
+		g2.draw(new Line2D.Double(320, 340, 360, 430)); // Right Leg
+	}
+	
 }
