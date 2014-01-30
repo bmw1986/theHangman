b695cb718c8a008e855582b83fbf153a1e4175b3
Freddy Oakes <freddy.oakes@gmail.com>
2014-01-30 15:29:11 -0500

    Added a testing branch with Hou's method of doing the problem started.

--- Main.java
+++ Main.java
@@ -1,47 +1,47 @@
 package thePackage;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.Line2D;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import thePackage.Paint;
+import thePackage.readFromFile;
 
-public class Main {
+@SuppressWarnings("serial")
+public class Main extends JPanel {
 	
+    static boolean head = false;  static boolean body = false;  static boolean leftArm = false;
+    static boolean rightArm = false;  static boolean leftLeg = false;  static boolean rightLeg = false;
+	
+    public void paintStage (Graphics g) {		
+    	super.paintComponent(g);
+    	Graphics2D g2 = (Graphics2D) g;
+ 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+ 		g2.setPaint(Color.gray);
+	
+ 		g2.draw(new Line2D.Double(320, 100, 480, 100)); // Top Beam
+ 		g2.draw(new Line2D.Double(320, 100, 320, 160)); // Rope
+ 		g2.draw(new Line2D.Double(200, 480, 600, 480));	// Bottom Line
+    }
+    
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader in = null;
-		String[] theWords = new String[10];
-		try {
-			in = new BufferedReader(new FileReader("/Volumes/The Data/Freddy/theWords.txt"));
-			
-			int index = 0;
-			String l;
-			while ((l = in.readLine()) != null) {
-				System.out.println(l);
-				theWords[index] = l;
-				index++;
-			}			
-		} finally {
-			if (in != null) 
-				in.close();
-		}
-		
-		int randomNum = 0 + (int)(Math.random()*10);
-		
-		String theWord = theWords[randomNum];
+		String theWord = readFromFile.findWord();
+	   	System.out.println(theWord);
 		
 		JFrame theFrame = new JFrame();
+		theFrame.repaint();
 		
-//		Paint.paintStage(null);
-//		
-//		theFrame.repaint();
-//		
 //		KeyListener keyListener = new KeyListener() {
 //			
 //			@Override
@@ -49,34 +49,28 @@ public class Main {
 //				int key = e.getKeyCode();
 //				
 //				if (key == KeyEvent.VK_1) {
-//					System.out.println("You have pressed an \"1\" on the keyboard!");
-//					Paint.paintHead(null);
-////					theFrame.repaint();
+//					System.out.println("Head");
+//					head = true;
 //				}
 //				if (key == KeyEvent.VK_2) {
-//					System.out.println("You have pressed an \"2\" on the keyboard!");
-//					Paint.paintBody(null);
-////					theFrame.repaint();
+//					System.out.println("Body!");
+//					body = true;
 //				}
 //				if (key == KeyEvent.VK_3) {
-//					System.out.println("You have pressed an \"3\" on the keyboard!");
-//					Paint.paintLeftArm(null);
-////					theFrame.repaint();
+//					System.out.println("leftArm");
+//					leftArm = true;
 //				}
 //				if (key == KeyEvent.VK_4) {
-//					System.out.println("You have pressed an \"4\" on the keyboard!");
-//					Paint.paintRightArm(null);
-////					theFrame.repaint();
+//					System.out.println("rightArm");
+//					rightArm = true;
 //				}
 //				if (key == KeyEvent.VK_5) {
-//					System.out.println("You have pressed an \"5\" on the keyboard!");
-//					Paint.paintLeftLeg(null);
-////					theFrame.repaint();
+//					System.out.println("leftLeg");
+//					leftLeg = true;
 //				}
 //				if (key == KeyEvent.VK_6) {
-//					System.out.println("You have pressed an \"6\" on the keyboard!");
-//					Paint.paintRightLeg(null);
-////					theFrame.repaint();
+//					System.out.println("rightLeg");
+//					rightLeg = true;
 //				}
 //			}
 //			
@@ -95,6 +89,5 @@ public class Main {
 		theFrame.pack();
 	    theFrame.setSize(new Dimension(800, 600));
 	    theFrame.setVisible(true);
-
 	}
 }
