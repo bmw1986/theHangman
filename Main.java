2549f752f2fbbdfccba53939bbbf9349273fd581
Freddy Oakes <freddy.oakes@me.com>
2014-01-31 13:21:15 -0500

    Program prevents user from enter the same char twice. However it prevents user from enter anything more than once. lol

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ package thePackage;
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-<<<<<<< HEAD
 import java.io.IOException;
 
 import javax.swing.JFrame;
@@ -31,74 +30,81 @@ public class Main extends JPanel {
 			private boolean CorrectOrNot;	
 			private int currentBodyPart = 0;	
 			private int winnerYet = 0;
-			private int lettersInArray = 0; 
+			private int doneYet = 0;
+			private int lettersInArray = 0;
 			private boolean keepGoing = true;
 			
-			for (int i=0; i<19; i++) {
-				usedLetters[i] = " ";
-			}
 			
 			@Override
 			public void keyReleased(KeyEvent e) {
-	//			int key = e.getKeyCode();
-	////			CorrectOrNot = GameLogic.isItCorrect(key, theWord);
-	//			
-	//			String letter = Character.toString((char) (key+32));
-	//			
-	////			if (lettersInArray > 1 ) {
-	////				for (int i=0; i < (lettersInArray-1); i++) {
-	////				}
-	////					if (letter.equals(usedLetters[lettersInArray]))
-	////						keepGoing = false;
-	////			}
-	//			
-	//			if (keepGoing = true) {
-	//				
-	//				usedLetters[lettersInArray] = letter;
-	//				lettersInArray++;
-	//				System.out.println(letter);
-	//				CorrectOrNot = theWord.contains(letter);
-	//				
-	//				if (CorrectOrNot == false) {
-	//					if (currentBodyPart < 5) {
-	//						currentBodyPart++;
-	//						System.out.println(currentBodyPart);
-	//					} else
-	//						currentBodyPart++;
-	//						System.out.println("YOU LOSE HAHAHAHAH");
-	//				}
-	//				else {
-	//					winnerYet++;
-	//					if (lengthOfWord == winnerYet)
-	//						System.out.println("YOU'VE WON");
-	//					else
-	//						System.out.println("Correct Letter Pressed");
-	//				}
-	//				
-	//				if (currentBodyPart == 1) {
-	//					Paint.getStatusOfPerson(1);
-	//					theFrame.repaint(); }
-	//				
-	//				if (currentBodyPart == 2) {
-	//					Paint.getStatusOfPerson(2);
-	//					theFrame.repaint(); }
-	//				
-	//				if (currentBodyPart == 3) {
-	//					Paint.getStatusOfPerson(3);
-	//					theFrame.repaint(); }
-	//				
-	//				if (currentBodyPart == 4) {
-	//					Paint.getStatusOfPerson(4);
-	//					theFrame.repaint(); }
-	//				
-	//				if (currentBodyPart == 5) {
-	//					Paint.getStatusOfPerson(5);
-	//					theFrame.repaint(); }
-	//				
-	//				if (currentBodyPart == 6) {
-	//					Paint.getStatusOfPerson(6);
-	//					theFrame.repaint(); }
-	//			}
+				int key = e.getKeyCode();
+				
+				String letter = Character.toString((char) (key+32));
+				
+				outerLoop:
+				if (lettersInArray >= 0 ) {
+					for (int i=0; i < (lettersInArray); i++) {
+						if (letter.equals(usedLetters[i])) {
+							keepGoing = false;
+							System.out.println("FALSE");
+							break outerLoop;
+							
+						}
+					}
+				}
+				
+				if (keepGoing == true) {
+					
+					usedLetters[lettersInArray] = letter;
+					lettersInArray = lettersInArray + 1;
+					System.out.println(letter);
+					CorrectOrNot = theWord.contains(letter);
+					
+					if (CorrectOrNot == false) {
+						if (currentBodyPart < 5) {
+							currentBodyPart++;
+							System.out.println(currentBodyPart);
+						} else {
+							currentBodyPart++;
+							System.out.println("YOU LOSE HAHAHAHAH");
+							doneYet = 2;
+						}
+					}
+					else {
+						winnerYet++;
+						if (lengthOfWord == winnerYet) {
+							System.out.println("YOU'VE WON");
+							doneYet = 1; }
+						else
+							System.out.println("Correct Letter Pressed");
+					}
+					
+					if (currentBodyPart == 1) {
+						Paint.getStatusOfPerson(1, doneYet);
+						theFrame.repaint(); }
+					
+					if (currentBodyPart == 2) {
+						Paint.getStatusOfPerson(2, doneYet);
+						theFrame.repaint(); }
+					
+					if (currentBodyPart == 3) {
+						Paint.getStatusOfPerson(3, doneYet);
+						theFrame.repaint(); }
+					
+					if (currentBodyPart == 4) {
+						Paint.getStatusOfPerson(4, doneYet);
+						theFrame.repaint(); }
+					
+					if (currentBodyPart == 5) {
+						Paint.getStatusOfPerson(5, doneYet);
+						theFrame.repaint(); }
+					
+					if (currentBodyPart == 6) {
+						Paint.getStatusOfPerson(6, doneYet);
+						theFrame.repaint(); }
+				
+					keepGoing = true;	
+				}
 			}
 			
 			@Override
@@ -117,100 +123,5 @@ public class Main extends JPanel {
 	    theFrame.setSize(new Dimension(800, 600));
 	    theFrame.setContentPane(new Paint());
 	    theFrame.setVisible(true);
-=======
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-import javax.swing.JFrame;
-
-import thePackage.Paint;
-
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader in = null;
-		String[] theWords = new String[10];
-		try {
-			in = new BufferedReader(new FileReader("/Volumes/The Data/Freddy/theWords.txt"));
-			
-			int index = 0;
-			String l;
-			while ((l = in.readLine()) != null) {
-				System.out.println(l);
-				theWords[index] = l;
-				index++;
-			}			
-		} finally {
-			if (in != null) 
-				in.close();
-		}
-		
-		int randomNum = 0 + (int)(Math.random()*10);
-		
-		String theWord = theWords[randomNum];
-		
-		JFrame theFrame = new JFrame();
-		
-//		Paint.paintStage(null);
-//		
-//		theFrame.repaint();
-//		
-//		KeyListener keyListener = new KeyListener() {
-//			
-//			@Override
-//			public void keyReleased(KeyEvent e) {
-//				int key = e.getKeyCode();
-//				
-//				if (key == KeyEvent.VK_1) {
-//					System.out.println("You have pressed an \"1\" on the keyboard!");
-//					Paint.paintHead(null);
-////					theFrame.repaint();
-//				}
-//				if (key == KeyEvent.VK_2) {
-//					System.out.println("You have pressed an \"2\" on the keyboard!");
-//					Paint.paintBody(null);
-////					theFrame.repaint();
-//				}
-//				if (key == KeyEvent.VK_3) {
-//					System.out.println("You have pressed an \"3\" on the keyboard!");
-//					Paint.paintLeftArm(null);
-////					theFrame.repaint();
-//				}
-//				if (key == KeyEvent.VK_4) {
-//					System.out.println("You have pressed an \"4\" on the keyboard!");
-//					Paint.paintRightArm(null);
-////					theFrame.repaint();
-//				}
-//				if (key == KeyEvent.VK_5) {
-//					System.out.println("You have pressed an \"5\" on the keyboard!");
-//					Paint.paintLeftLeg(null);
-////					theFrame.repaint();
-//				}
-//				if (key == KeyEvent.VK_6) {
-//					System.out.println("You have pressed an \"6\" on the keyboard!");
-//					Paint.paintRightLeg(null);
-////					theFrame.repaint();
-//				}
-//			}
-//			
-//			@Override
-//			public void keyPressed(KeyEvent e) {
-//				// TODO Auto-generated method stub
-//			} 
-//			
-//			@Override
-//			public void keyTyped(KeyEvent e) {
-//				// TODO Auto-generated method stub
-//			}
-//		};
-//		
-//		theFrame.addKeyListener(keyListener);
-		theFrame.pack();
-	    theFrame.setSize(new Dimension(800, 600));
-	    theFrame.setVisible(true);
-
->>>>>>> 4547088d1d450e695252eeb45262a96cc528c03a
 	}
 }
