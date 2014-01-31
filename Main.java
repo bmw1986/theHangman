9a38ea7f9100531868a53eef5d23fbc214ac7891
Freddy Oakes <freddy.oakes@me.com>
2014-01-31 17:14:28 -0500

    Added some libraries for RS232 communication. All code for RS232 has been commented out for the time being.

--- Main.java
+++ Main.java
@@ -3,10 +3,16 @@ package thePackage;
 import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.comm.*;
+
+import java.util.*;
 
 @SuppressWarnings("serial")
 public class Main extends JPanel {
@@ -14,8 +20,67 @@ public class Main extends JPanel {
     static boolean head = false;  static boolean body = false;  static boolean leftArm = false;
     static boolean rightArm = false;  static boolean leftLeg = false;  static boolean rightLeg = false;
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, UnsupportedCommOperationException {
 
+//		String wantedPortName = "/dev/tty1";
+//		@SuppressWarnings("rawtypes")
+//		Enumeration portIdentifiers = CommPortIdentifier.getPortIdentifiers();
+//		
+//		CommPortIdentifier portId = null;  // will be set if port found
+//		while (portIdentifiers.hasMoreElements())
+//		{
+//		    CommPortIdentifier pid = (CommPortIdentifier) portIdentifiers.nextElement();
+//		    if(pid.getPortType() == CommPortIdentifier.PORT_SERIAL &&
+//		       pid.getName().equals(wantedPortName)) 
+//		    {
+//		        portId = pid;
+//		        break;
+//		    }
+//		}
+//		if(portId == null)
+//		{
+//		    System.err.println("Could not find serial port " + wantedPortName);
+//		    System.exit(1);
+//		}
+//		SerialPort port = null;
+//		try {
+//		    port = (SerialPort) portId.open(
+//		        "name", // Name of the application asking for the port 
+//		        10000   // Wait max. 10 sec. to acquire port
+//		    );
+//		} catch(PortInUseException e) {
+//		    System.err.println("Port already in use: " + e);
+//		    System.exit(1);
+//		}
+//		
+//		port.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+//
+//		BufferedReader is = null;  // for demo purposes only. A stream would be more typical.
+//		PrintStream    os = null;
+//
+//		try {
+//		  is = new BufferedReader(new InputStreamReader(port.getInputStream()));
+//		} catch (IOException e) {
+//		  System.err.println("Can't open input stream: write-only");
+//		  is = null;
+//		}
+//		
+//		os = new PrintStream(port.getOutputStream(), true);
+//		
+//		if (is != null) is.close();
+//		if (os != null) os.close();
+//		if (port != null) port.close();
+//		
+//		os.print("AT");
+//		os.print("\r\n"); // Append a carriage return with a line feed
+//
+//		is.readLine(); // First read will contain the echoed command you sent to it. In this case: "AT"
+//		is.readLine(); // Second read will remove the extra line feed that AT generates as output
+//
+//		String response = is.readLine(); // if you sent "AT" then response == "OK"
+//		
+//		System.out.println(response);
+		
 		final JFrame theFrame = new JFrame();
 		
 		final String theWord = ReadFromFile.findWord();
@@ -110,12 +175,12 @@ public class Main extends JPanel {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				// TODO Auto-generated method stub
-			} 
-			
+				
+			}
 			@Override
 			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-			}
+				// TODO Auto-generated method stub 
+			}			
 		};
 		
 		theFrame.addKeyListener(keyListener);
